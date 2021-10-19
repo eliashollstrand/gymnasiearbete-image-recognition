4faad7d5bc7def343db29d5c3f5191544b7aa23c
Elias Hollstrand <elias.hollstrand@icloud.com>
2021-10-19 11:28:24 +0200

    Added trained model & detection for one class

---
+++ detection.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+from tensorflow.keras.applications.mobilenet import preprocess_input
+from tensorflow.keras.preprocessing import image
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = "images/test/Elias.b195b325-211b-11ec-81af-84fdd18c7552.jpg"
+# plt.imshow(img)
+# plt.show()
+
+img = image.load_img(img, target_size=(224, 224))
+
+img_array = image.img_to_array(img)
+img_batch = np.expand_dims(img_array, axis=0)
+
+img_preprocessed = preprocess_input(img_batch)
+
+model = tf.keras.models.load_model('saved model/fine_tuning.h5')
+model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+prediction = model.predict(img_preprocessed)
+print(prediction)
+
+classes = np.argmax(prediction, axis = 1)
+detected_class_index = classes[0]-1
+
+labels = open("labels.txt", 'r')
+
+for position, line in enumerate(labels):
+    if position == detected_class_index:
+        print("Detected: " + line)
+
